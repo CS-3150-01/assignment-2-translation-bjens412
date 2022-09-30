2ccb9a5a3470763c6d5c99a05eceb56fd19e527e
Britton Jensen <brittonjensen@Brittons-MacBook-Air.local>
2022-09-30 12:53:26 -0600

    test

--- C/BinarySearchTree.c
+++ C/BinarySearchTree.c
@@ -1,5 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+}NODE;
+
+ NODE* newNode(int d){
+   NODE* result = malloc(sizeof(NODE));
+   if(result != NULL){
+    result -> data = d;
+    result -> left = NULL;
+    result -> right = NULL;
+   }
+   return result;
+}
+
+
+NODE* root;
+
+void BinarySearchTree(){
+    root = NULL;
+}
+
+int factorial(int num){
+    int fact = 1;
+    if (num == 0){
+        return 1;
+    }
+    else {
+        while (num > 1){
+            fact = fact * num;
+            num--;
+        }
+        return fact;
+    }
+}
+
+int numOfBST(int key){
+    int catalanNumber = factorial(2 * key)/(factorial(key + 1) * factorial(key));
+    return catalanNumber;
+}
+
+
 
 int main() {
+
+    printf("Total number of possible Binary Trees with given key: %i. \n", numOfBST(5));
     return 0;
 }
--- C/SinglyLinkedList.c
+++ C/SinglyLinkedList.c
@@ -1,5 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node *next;
+}NODE;
+
+NODE* newNode(int d){
+    NODE* result = malloc(sizeof(NODE));
+    if (result != NULL){
+        result -> data = d;
+        result -> next = NULL;
+    }
+
+    return result;
+}
+
+NODE* head;
+NODE* tail;
+
+void addNode(int d){
+    NODE* babyNode = newNode(d);
+
+    if(head == NULL){
+        head = babyNode;
+        tail = babyNode;
+    }
+    else{
+        tail -> next = babyNode;
+        tail = babyNode;
+    }
+}
+
+void display(){
+    NODE* current = head;
+
+    if(head == NULL){
+        printf("List is empty");
+        return;
+    }
+    printf("Nodes of singly linked list: ");
+    while(current != NULL){
+        printf("%i \n", current -> data);
+        current = current -> next;
+    }
+    printf("\n");
+}
 
 int main() {
+
+    addNode(1);
+    addNode(2);
+    addNode(3);
+    addNode(4);
+    
+    display();
     return 0;
 }
